953052e4ff41b364df3fc598e0111a5b416776b1
Dan Morris <227039716+damorris72@users.noreply.github.com>
2026-08-11 18:13:14 -0500

    Build-AI-IQ-Increaser-MVP

--- frontend/pages/api/hello.js
+++ frontend/pages/api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(request, response) {
-  response.status(200).json({ message: "AI Gateway API is working." });
+  response.status(200).json({ message: "AI IQ Increaser API is working." });
 }
